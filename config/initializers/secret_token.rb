66f8590fb5a1db98acbe45589e3e9659cfc97681
Jaime Victoria <jaime.victoria@crowdint.com>
2013-08-29 18:57:26 -0500

    First commit
    Signed-off-by: Eduardo Figarola <eduardo.figarola@crowdint.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helloworld::Application.config.secret_key_base = 'c24eede83f89b1e7fed53569ac8fc8ea6a745fafee4bd404f68a479aa2c86769f1ff2756da1d67e6f227c02c04c4db4825f103046f34652f65f99a78dfc1c68e'
